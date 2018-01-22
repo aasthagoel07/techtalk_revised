5d2d96a22cfe8532e636124af906a6a1c6e9945d
Aastha Goel <aastha.goel@cybergindia.local>
2018-01-22 16:11:21 +0530

    Modified event api

--- techtalk_revised/Controllers/eventController.cs
+++ techtalk_revised/Controllers/eventController.cs
@@ -1,13 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using techtalk_revised.Models;
 
 namespace techtalk_revised.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class eventController : ApiController
     {
+        private techtalkEntities db = new techtalkEntities();
+        public IQueryable<tevent> GetPastEvent()
+        {
+            var todaydate = DateTime.Now;
+            var pastEvents = from s in db.tevents where s.scheduledOn < todaydate orderby s.scheduledOn descending select s;
+            return pastEvents;
+
+        }
+        public IQueryable<tevent> GetUpcomingEvent()
+        {
+            var todaydate = DateTime.Now;
+            var upcomingEvents = from s in db.tevents where s.scheduledOn > todaydate orderby s.scheduledOn descending select s;
+            return upcomingEvents;
+
+        }
+        [ResponseType(typeof(tevent))]
+        public IHttpActionResult PostEventTable(tevent eventTable)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tevents.Add(eventTable);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (EventTableExists(eventTable.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = eventTable.eventID }, eventTable);
+        }
+        // PUT: api/EventTables/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEventTable(int id, tevent eventTable)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != eventTable.eventID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(eventTable).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventTableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        // DELETE: api/EventTables/5
+        [ResponseType(typeof(tevent))]
+        public IHttpActionResult DeleteEventTable(int id)
+        {
+            tevent eventTable = db.tevents.Find(id);
+            if (eventTable == null)
+            {
+                return NotFound();
+            }
+
+            db.tevents.Remove(eventTable);
+            db.SaveChanges();
+
+            return Ok(eventTable);
+        }
+        private bool EventTableExists(int id)
+        {
+            return db.tevents.Count(e => e.eventID == id) > 0;
+        }
     }
 }
