b0f793f5933fb30ad2477f3b7c4d7381841087e6
Zeeshan Al Abedin <zeeshan.abedin@cybergindia.local>
2018-01-22 13:21:06 +0530

    get user route made

--- techtalk_revised/Controllers/userController.cs
+++ techtalk_revised/Controllers/userController.cs
@@ -4,13 +4,56 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using techtalk_revised.Models;
 
 namespace techtalk_revised.Controllers
 {
     public class userController : ApiController
     {
+        //Db entity
+        private techtalkEntities db = new techtalkEntities();
 
+        //GET all users
+        [HttpGet]
 
+        public IHttpActionResult getUsers()
+        {
+            return Ok(db.users);
+        }
+
+        //GET users via ID
+        [HttpGet]
+        public IHttpActionResult getUsers(int id)
+        {
+            user userID = db.users.Find(id);
+            if(userID == null)
+            {
+                return NotFound();
+            }
+            return Ok(db.users);
+        }
+       
+        //POST Insert User
+        [HttpPost]
+
+        public IHttpActionResult postUser(user user1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.users.Add(user1);
+            try
+            {
+                db.SaveChanges();
+                return Ok(user1);
+            }
+            catch(Exception e)
+            {
+                return Ok(e.StackTrace);
+            }
+        }
 
 
 
